ec312b23828623ffc344b923407c20b0bfa8934e
Przemek Rękawiecki <przemek.rek@gmail.com>
2014-08-02 23:13:49 +0200

    server can manage client connections and pass requests between endpoints

---
+++ rover_server/server.py
@@ -0,0 +1,255 @@
+import sys
+import argparse
+
+from twisted.python import log
+from twisted.internet.protocol import Factory
+from twisted.protocols.basic import LineReceiver
+from twisted.internet import reactor
+
+
+class ProtocolConnections(object):
+    """
+    client request structure:
+    <command>:<request-body>
+
+    available client commands:
+    DC - device is connecting
+    CC - controller is connecting
+        on success server in response sends DL
+    CD - controller selects device to connect
+        on success server in response sends CD:OK
+    RE - controller sends communicate to connected device
+        request format:
+        RE:<REQUEST>
+
+    server request structure:
+    <command>:<request-body>
+    available server commands to clients:
+    RE - server sends communicate sent from connected endpoint
+
+    DL - devices list available for connetion
+    SE - client request error
+
+    errors:
+    E_10 - invalid client command
+    E_20 - no device connected
+    E_30 - no controller connected
+
+    """
+
+    devices = set()
+    controllers = set()
+    protocols = {}
+
+    @classmethod
+    def line_received(clk, protocol, line):
+
+        command = clk.get_command(line)
+        body = clk.get_body(line)
+
+        # the device is connecting
+        if command == 'DC':
+            clk.connect_device(protocol, body)
+
+        # the contorller is connecting
+        elif command == 'CC':
+            clk.connect_controller(protocol, body)
+
+        # controller is connecting to selected device
+        elif command == 'CD':
+            device_name = body
+            device_protocol = clk.protocols[device_name]
+            clk.make_connection(device_protocol, protocol)
+
+            protocol.sendLine('CD:OK')
+
+        elif command == 'RE':
+            device_protocol = clk.protocols[protocol.get_endpoint()]
+            device_protocol.sendLine('RE:' + body)
+
+        # elif command == 'DR':
+        #     controller_protocol = clk.protocols[protocol.get_endpoint()]
+        #     controller_protocol.sendLine('DR:' + body)
+
+        #invalid client request
+        else:
+            protocol.sendLine('SE:E_10')
+
+    @classmethod
+    def get_command(clk, line):
+        return line[:2]
+
+    @classmethod
+    def get_body(clk, line):
+        return line[3:]
+
+    @classmethod
+    def connect_device(clk, device_protocol, device_name):
+        device_protocol.name = device_name
+        clk.devices.add(device_name)
+        clk.protocols[device_name] = device_protocol
+        log.msg("device {} is connected", device_name)
+
+        #notify all controllers about new device
+        clk.notify_all_about_available_devices()
+
+    @classmethod
+    def notify_all_about_available_devices(clk):
+        #notify all controllers about new device
+        if clk.controllers:
+            devices_available = clk.get_available_devices()
+            for controller_name in clk.controllers:
+                clk.notify_about_available_devices(
+                    clk.protocols[controller_name],
+                    devices_available
+                )
+
+    @classmethod
+    def notify_about_available_devices(clk, protocol, devices_available):
+        response_line = 'DL:' + ':'.join(devices_available)
+        protocol.sendLine(response_line)
+
+        log.msg(
+            'devicec available: {} sent to {}'.format(
+                response_line,
+                protocol.name,
+            )
+        )
+
+    @classmethod
+    def get_available_devices(clk):
+        available_devices = []
+        for device_name in clk.devices:
+            dev_protocol = clk.protocols[device_name]
+            if dev_protocol.get_endpoint() is None:
+                available_devices.append(device_name)
+
+        return available_devices
+
+    @classmethod
+    def connect_controller(clk, controller_protocol, controller_name):
+        controller_protocol.name = controller_name
+        clk.controllers.add(controller_name)
+        clk.protocols[controller_name] = controller_protocol
+        print "controller is connected"
+
+        #respond with list of connected devices
+        devices_available = clk.get_available_devices()
+        clk.notify_about_available_devices(
+            controller_protocol, devices_available
+        )
+
+
+
+    @classmethod
+    def make_connection(clk, device_protocol, controller_protocol):
+        device_protocol.connect_endpoint(controller_protocol.name)
+        controller_protocol.connect_endpoint(device_protocol.name)
+        clk.notify_all_about_available_devices()
+
+    @classmethod
+    def disconnect_protocol(clk, protocol):
+        log.msg('disconnecting protocol {}'.format(protocol.name))
+
+        if protocol.name in clk.devices:
+            end_protocol = clk.protocols.get(protocol.get_endpoint(), None)
+            if end_protocol is not None:
+                end_protocol.disconnect_endpoint()
+                end_protocol.sendLine('DD:')
+
+            clk.devices.remove(protocol.name)
+            clk.notify_all_about_available_devices()
+
+        elif protocol.name in clk.controllers:
+            end_protocol = clk.protocols.get(protocol.get_endpoint(), None)
+            if end_protocol is not None:
+                end_protocol.disconnect_endpoint()
+                clk.notify_all_about_available_devices()
+
+            clk.controllers.remove(protocol.name)
+        else:
+            log.err('ERROR - protocol {} neither in controllers nor devices'.\
+                    format(protocol.name)
+            )
+
+        if clk.protocols.pop(protocol.name, None) is None:
+            log.err('ERROR - protocol {} not in protocols'.\
+                    format(protocol.name)
+            )
+
+        protocol.disconnect_endpoint()
+
+    @classmethod
+    def reset(clk):
+
+        for p in clk.protocols:
+            clk.protocols[p].reset()
+
+        clk.devices = set()
+        clk.controllers = set()
+        clk.protocols = {}
+
+
+class ServerProtocol(LineReceiver):
+
+    name = None
+    endpoint = None
+
+    def connectionMade(self):
+        print "connection from a client made"
+
+    def connectionLost(self, reason):
+        log.msg("connection lost: {}".format(reason))
+
+        #disconect from endpoint
+        ProtocolConnections.disconnect_protocol(self)
+
+    def lineReceived(self, line):
+        print line
+
+        ProtocolConnections.line_received(self, line)
+
+    def reset(self):
+        self.name = None
+        self.disconnect_endpoint()
+
+    def connect_endpoint(self, protocol):
+        self.endpoint = protocol
+
+    def disconnect_endpoint(self):
+        self.endpoint = None
+
+    def get_endpoint(self):
+        return self.endpoint
+
+
+class ServerFactory(Factory):
+
+    def buildProtocol(self, addr):
+        return ServerProtocol()
+
+
+def main():
+    DEFAULT_PORT = 8123
+
+    log.startLogging(sys.stdout)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p", "--port",
+        help="port on which server will listen",
+        action="store_true"
+    )
+
+    args = parser.parse_args()
+
+    port = DEFAULT_PORT
+    if args.port:
+        port = args.port
+
+    reactor.listenTCP(port, ServerFactory())
+    reactor.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests.py
@@ -0,0 +1,387 @@
+from twisted.trial import unittest
+from twisted.test import proto_helpers
+
+from rover_server.server import ServerFactory
+from rover_server.server import ServerProtocol
+from rover_server.server import ProtocolConnections
+
+
+END_LINE = '\r\n'
+DEVICE_NAME = 'mock-client'
+CONTROLLER_NAME = 'mock-controller'
+
+
+class ProtocolConnectionsLineRecievedTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.tr_device = proto_helpers.StringTransport()
+        self.proto_device = ServerProtocol()
+        self.proto_device.makeConnection(self.tr_device)
+
+        self.proto_controller = ServerProtocol()
+        self.tr_controller = proto_helpers.StringTransport()
+        self.proto_controller.makeConnection(self.tr_controller)
+
+    def tearDown(self):
+        ProtocolConnections.reset()
+
+    def test_new_device_connecting(self):
+
+        EXPECTED_R_FOR_C = ''
+        EXPECTED_R_FOR_D = ''
+
+        REQUEST = 'DC:' + DEVICE_NAME + END_LINE
+        self.proto_device.dataReceived(REQUEST)
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, EXPECTED_R_FOR_C)
+        self.assertEqual(r_for_device, EXPECTED_R_FOR_D)
+
+        self.assertEqual(len(ProtocolConnections.devices), 1)
+        self.assertEqual(len(ProtocolConnections.controllers), 0)
+        assert DEVICE_NAME in ProtocolConnections.devices
+
+        self.assertEqual(len(ProtocolConnections.protocols), 1)
+        self.assertEqual(ProtocolConnections.protocols[DEVICE_NAME],
+                         self.proto_device)
+
+    def test_new_controller_connecting(self):
+
+        EXPECTED_R_FOR_C = 'DL:' + END_LINE
+        EXPECTED_R_FOR_D = ''
+
+        REQUEST = 'CC:' + CONTROLLER_NAME + END_LINE
+        self.proto_controller.dataReceived(REQUEST)
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, EXPECTED_R_FOR_C)
+        self.assertEqual(r_for_device, EXPECTED_R_FOR_D)
+
+        self.assertEqual(len(ProtocolConnections.controllers), 1)
+        self.assertEqual(len(ProtocolConnections.devices), 0)
+        assert CONTROLLER_NAME in ProtocolConnections.controllers
+
+        self.assertEqual(len(ProtocolConnections.protocols), 1)
+        self.assertEqual(ProtocolConnections.protocols[CONTROLLER_NAME],
+                         self.proto_controller)
+
+    def test_controller_connects_after_device(self):
+
+        EXPECTED_R_FOR_C = 'DL:' + DEVICE_NAME + END_LINE
+        EXPECTED_R_FOR_D = ''
+
+        #device is connecting
+        D_REQUEST = 'DC:' + DEVICE_NAME + END_LINE
+        self.proto_device.dataReceived(D_REQUEST)
+
+        #controller is connecting
+        C_REQUEST = 'CC:' + CONTROLLER_NAME + END_LINE
+        self.proto_controller.dataReceived(C_REQUEST)
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, EXPECTED_R_FOR_C)
+        self.assertEqual(r_for_device, EXPECTED_R_FOR_D)
+
+        self.assertEqual(len(ProtocolConnections.controllers), 1)
+        self.assertEqual(len(ProtocolConnections.devices), 1)
+        assert CONTROLLER_NAME in ProtocolConnections.controllers
+
+        self.assertEqual(len(ProtocolConnections.protocols), 2)
+        self.assertEqual(ProtocolConnections.protocols[DEVICE_NAME],
+                         self.proto_device)
+        self.assertEqual(ProtocolConnections.protocols[CONTROLLER_NAME],
+                         self.proto_controller)
+
+    def test_device_connects_after_controller(self):
+
+        EXPECTED_R_FOR_C = 'DL:' + END_LINE
+        EXPECTED_R_FOR_C += 'DL:' + DEVICE_NAME + END_LINE
+        EXPECTED_R_FOR_D = ''
+
+        #controller is connecting
+        C_REQUEST = 'CC:' + CONTROLLER_NAME + END_LINE
+        self.proto_controller.dataReceived(C_REQUEST)
+
+        #device is connecting
+        D_REQUEST = 'DC:' + DEVICE_NAME + END_LINE
+        self.proto_device.dataReceived(D_REQUEST)
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, EXPECTED_R_FOR_C)
+        self.assertEqual(r_for_device, EXPECTED_R_FOR_D)
+
+        self.assertEqual(len(ProtocolConnections.controllers), 1)
+        self.assertEqual(len(ProtocolConnections.devices), 1)
+        assert CONTROLLER_NAME in ProtocolConnections.controllers
+
+        self.assertEqual(len(ProtocolConnections.protocols), 2)
+        self.assertEqual(ProtocolConnections.protocols[DEVICE_NAME],
+                         self.proto_device)
+        self.assertEqual(ProtocolConnections.protocols[CONTROLLER_NAME],
+                         self.proto_controller)
+
+    def test_device_disconnects_after_controller_connects(self):
+
+        EXPECTED_R_FOR_C = 'DL:' + DEVICE_NAME + END_LINE
+        EXPECTED_R_FOR_C += 'DL:' + END_LINE
+        EXPECTED_R_FOR_D = ''
+
+        #device is connecting
+        D_REQUEST = 'DC:' + DEVICE_NAME + END_LINE
+        self.proto_device.dataReceived(D_REQUEST)
+
+        #controller is connecting
+        C_REQUEST = 'CC:' + CONTROLLER_NAME + END_LINE
+        self.proto_controller.dataReceived(C_REQUEST)
+
+        #device is disconnected
+        self.proto_device.connectionLost('network failure')
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, EXPECTED_R_FOR_C)
+        self.assertEqual(r_for_device, EXPECTED_R_FOR_D)
+
+        self.assertEqual(len(ProtocolConnections.controllers), 1)
+        self.assertEqual(len(ProtocolConnections.devices), 0)
+        assert CONTROLLER_NAME in ProtocolConnections.controllers
+
+        self.assertEqual(len(ProtocolConnections.protocols), 1)
+        self.assertEqual(ProtocolConnections.protocols[CONTROLLER_NAME],
+                         self.proto_controller)
+
+    def test_controller_connects_to_device(self):
+        EXPECTED_R_FOR_C = 'DL:' + DEVICE_NAME + END_LINE
+        EXPECTED_R_FOR_C += 'DL:' + END_LINE
+        EXPECTED_R_FOR_C += 'CD:OK' + END_LINE
+        EXPECTED_R_FOR_D = ''
+
+        #device is connecting
+        DC_REQUEST = 'DC:' + DEVICE_NAME + END_LINE
+        self.proto_device.dataReceived(DC_REQUEST)
+
+        #controller is connecting
+        CC_REQUEST = 'CC:' + CONTROLLER_NAME + END_LINE
+        self.proto_controller.dataReceived(CC_REQUEST)
+
+        #controller connects to selected device
+        CD_REQUEST = 'CD:' + DEVICE_NAME + END_LINE
+        self.proto_controller.dataReceived(CD_REQUEST)
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, EXPECTED_R_FOR_C)
+        self.assertEqual(r_for_device, EXPECTED_R_FOR_D)
+
+        self.assertEqual(len(ProtocolConnections.controllers), 1)
+        self.assertEqual(len(ProtocolConnections.devices), 1)
+        assert CONTROLLER_NAME in ProtocolConnections.controllers
+
+        self.assertEqual(len(ProtocolConnections.protocols), 2)
+        self.assertEqual(ProtocolConnections.protocols[DEVICE_NAME],
+                         self.proto_device)
+        self.assertEqual(ProtocolConnections.protocols[CONTROLLER_NAME],
+                         self.proto_controller)
+
+    def test_device_connected_to_controller_disconetcts(self):
+
+        EXPECTED_R_FOR_C = 'DL:' + DEVICE_NAME + END_LINE
+        EXPECTED_R_FOR_C += 'DL:' + END_LINE
+        EXPECTED_R_FOR_C += 'CD:OK' + END_LINE
+        EXPECTED_R_FOR_C += 'DD:' + END_LINE
+        EXPECTED_R_FOR_C += 'DL:' + END_LINE
+
+        EXPECTED_R_FOR_D = ''
+
+        #device is connecting
+        DC_REQUEST = 'DC:' + DEVICE_NAME + END_LINE
+        self.proto_device.dataReceived(DC_REQUEST)
+
+        #controller is connecting
+        CC_REQUEST = 'CC:' + CONTROLLER_NAME + END_LINE
+        self.proto_controller.dataReceived(CC_REQUEST)
+
+        #controller connects to selected device
+        CD_REQUEST = 'CD:' + DEVICE_NAME + END_LINE
+        self.proto_controller.dataReceived(CD_REQUEST)
+
+        #device is disconnected
+        self.proto_device.connectionLost('network failure')
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, EXPECTED_R_FOR_C)
+        self.assertEqual(r_for_device, EXPECTED_R_FOR_D)
+
+        self.assertEqual(len(ProtocolConnections.controllers), 1)
+        self.assertEqual(len(ProtocolConnections.devices), 0)
+        assert CONTROLLER_NAME in ProtocolConnections.controllers
+
+        self.assertEqual(len(ProtocolConnections.protocols), 1)
+        self.assertEqual(ProtocolConnections.protocols[CONTROLLER_NAME],
+                         self.proto_controller)
+
+class ProtocolConnectionsRequestsTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.tr_device = proto_helpers.StringTransport()
+        self.proto_device = ServerProtocol()
+        self.proto_device.makeConnection(self.tr_device)
+
+        self.proto_controller = ServerProtocol()
+        self.tr_controller = proto_helpers.StringTransport()
+        self.proto_controller.makeConnection(self.tr_controller)
+
+        #device is connecting
+        DC_REQUEST = 'DC:' + DEVICE_NAME + END_LINE
+        self.proto_device.dataReceived(DC_REQUEST)
+
+        #controller is connecting
+        CC_REQUEST = 'CC:' + CONTROLLER_NAME + END_LINE
+        self.proto_controller.dataReceived(CC_REQUEST)
+
+        #controller connects to selected device
+        CD_REQUEST = 'CD:' + DEVICE_NAME + END_LINE
+        self.proto_controller.dataReceived(CD_REQUEST)
+
+        self.expected_r_for_c = 'DL:' + DEVICE_NAME + END_LINE
+        self.expected_r_for_c += 'DL:' + END_LINE
+        self.expected_r_for_c += 'CD:OK' + END_LINE
+
+        self.expected_r_for_d = ''
+
+    def tearDown(self):
+        ProtocolConnections.reset()
+
+    def test_controller_sends_request_to_device(self):
+
+        REQUEST = '0:0:0'
+
+        # self.expected_r_for_c += '' + END_LINE
+
+        self.expected_r_for_d += 'RE:' + REQUEST + END_LINE
+
+        #controller send request
+        CR_REQUEST = 'RE:' + REQUEST + END_LINE
+        self.proto_controller.dataReceived(CR_REQUEST)
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, self.expected_r_for_c)
+        self.assertEqual(r_for_device, self.expected_r_for_d)
+
+    def test_device_sends_request_to_controller(self):
+
+        REQUEST = '0:0:0'
+
+        self.expected_r_for_c += 'RE:' + REQUEST + END_LINE
+
+        #controller send request
+        DR_REQUEST = 'RE:' + REQUEST + END_LINE
+        self.proto_device.dataReceived(DR_REQUEST)
+
+        r_for_controller = self.tr_controller.value()
+        r_for_device = self.tr_device.value()
+
+        self.assertEqual(r_for_controller, self.expected_r_for_c)
+        self.assertEqual(r_for_device, self.expected_r_for_d)
+
+
+def proto_factory():
+    proto = ServerProtocol()
+    tr = proto_helpers.StringTransport()
+    proto.makeConnection(tr)
+
+    return proto, tr
+
+
+class ProtocolConnectionsUnitsTest(unittest.TestCase):
+
+    def tearDown(self):
+        ProtocolConnections.reset()
+
+    def test_get_available_devices(self):
+
+        avail_devs = ProtocolConnections.get_available_devices()
+        assert avail_devs == []
+
+        DEV1_NAME = 'mock-dev1'
+        proto_d_1, _ = proto_factory()
+        DEV2_NAME = 'mock-dev2'
+        proto_d_2, _ = proto_factory()
+        DEV3_NAME = 'mock-dev3'
+        proto_d_3, _ = proto_factory()
+        CON1_NAME = 'mock-con1'
+        proto_c_1, _ = proto_factory()
+
+        ProtocolConnections.connect_device(proto_d_1, DEV1_NAME)
+        avail_devs = ProtocolConnections.get_available_devices()
+        assert DEV1_NAME in avail_devs
+        assert len(avail_devs) == 1
+
+        ProtocolConnections.connect_device(proto_d_2, DEV2_NAME)
+        avail_devs = ProtocolConnections.get_available_devices()
+        assert DEV1_NAME in avail_devs
+        assert DEV2_NAME in avail_devs
+        assert len(avail_devs) == 2
+
+        ProtocolConnections.connect_device(proto_d_3, DEV3_NAME)
+        avail_devs = ProtocolConnections.get_available_devices()
+        assert DEV1_NAME in avail_devs
+        assert DEV2_NAME in avail_devs
+        assert DEV3_NAME in avail_devs
+        assert len(avail_devs) == 3
+
+        ProtocolConnections.connect_controller(proto_c_1, CON1_NAME)
+        ProtocolConnections.make_connection(proto_d_1, proto_c_1)
+        avail_devs = ProtocolConnections.get_available_devices()
+        assert DEV2_NAME in avail_devs
+        assert DEV3_NAME in avail_devs
+        assert len(avail_devs) == 2
+
+        ProtocolConnections.disconnect_protocol(proto_c_1)
+        avail_devs = ProtocolConnections.get_available_devices()
+        assert DEV1_NAME in avail_devs
+        assert DEV2_NAME in avail_devs
+        assert DEV3_NAME in avail_devs
+        assert len(avail_devs) == 3
+
+    def test_connect_device(self):
+
+        DEVICE_NAME = 'mock-dev'
+        proto_dev, _ = proto_factory()
+
+        CONTROLLER_NAME = 'mock-con'
+        proto_con, tr_con = proto_factory()
+
+        EXPECTED_R_FOR_C = 'DL:' + END_LINE
+        EXPECTED_R_FOR_C += 'DL:' + DEVICE_NAME + END_LINE
+
+        assert len(ProtocolConnections.devices) == 0
+        assert len(ProtocolConnections.protocols) == 0
+
+        ProtocolConnections.connect_controller(proto_con, CONTROLLER_NAME)
+        ProtocolConnections.connect_device(proto_dev, DEVICE_NAME)
+
+        assert len(ProtocolConnections.devices) == 1
+        assert len(ProtocolConnections.protocols) == 2
+        assert DEVICE_NAME in ProtocolConnections.devices
+        assert proto_dev == ProtocolConnections.protocols[DEVICE_NAME]
+
+        #make sure connected controllers get notified about new device
+        resp_for_controller = tr_con.value()
+        assert resp_for_controller == EXPECTED_R_FOR_C
